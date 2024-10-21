d5a257dcf4c3b01fcca5faf2178794a19e1589f1
techiemokhele <neomokhele23@gmail.com>
2024-10-21 15:08:36 +0200

    Refactor FeedComponent: add state for all posts, searched results, and search timeout; implement debounced search and filtering; update PromptCardListComponent props

--- components/FeedComponent.jsx
+++ components/FeedComponent.jsx
@@ -5,24 +5,52 @@ import { useState, useEffect } from "react";
 import PromptCardListComponent from "./PromptCardListComponent";
 
 const FeedComponent = () => {
-  const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
 
-  const handleSearchChange = (e) => {
-    e.preventDefault();
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
   };
 
   useEffect(() => {
-    const fetchPost = async () => {
-      const response = await fetch("/api/prompt");
-
-      const data = await response.json();
-
-      setPosts(data);
-    };
-
-    fetchPost();
+    fetchPosts();
   }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i");
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
     <section className="feed">
@@ -37,7 +65,17 @@ const FeedComponent = () => {
         />
       </form>
 
-      <PromptCardListComponent data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardListComponent
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardListComponent
+          data={allPosts}
+          handleTagClick={handleTagClick}
+        />
+      )}
     </section>
   );
 };
